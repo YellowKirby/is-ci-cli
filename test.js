ebac2c0b4d633f595c3119930d44a44fb7b70f9e
YellowKirby <YellowKirby@users.noreply.github.com>
2017-05-16 14:41:49 -0700

    Basic functionality works

---
+++ test.js
@@ -0,0 +1,24 @@
+import childProcess from 'child_process';
+import test from 'ava';
+import sinon from 'sinon';
+import run from './cli';
+
+test.before('stub spawn', () => {
+	sinon.stub(childProcess, 'spawn').callsFake((name, args) => args);
+});
+
+test.after('restore stub', () => {
+	childProcess.spawn.restore();
+});
+
+test('runs first argument when in CI environment', t => {
+	t.deepEqual(run(['first', 'second'], true), ['run', 'first']);
+});
+
+test('runs second argument when not in CI environment', t => {
+	t.deepEqual(run(['first', 'second'], false), ['run', 'second']);
+});
+
+test('runs nothing if no second argument and not in CI environment', t => {
+	t.is(run(['first'], false), undefined);
+});
