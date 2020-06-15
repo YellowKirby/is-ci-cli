97271ceac8ec2afc12beb16726426c471ccc0729
YellowKirby <YellowKirby@users.noreply.github.com>
2020-06-15 13:52:12 -0700

    simplify unit tests

--- cli.js
+++ cli.js
@@ -6,42 +6,46 @@ process.on('unhandledRejection', (err) => {
 	throw err;
 });
 
-const crossSpawn = require('cross-spawn');
+const spawn = require('cross-spawn');
 const isCi = require('is-ci');
 
 function isYarn(npmExec) {
 	return npmExec.includes('yarn');
 }
 
-function getScriptArgs(args, npmExec) {
-	const rawArgs = args.slice(2);
+function getSpawnArgs(
+	[ciScript, noCiScript, ...additionalArgs],
+	isCi,
+	npmExec
+) {
+	const script = isCi ? ciScript : noCiScript;
+	if (!script || script.trim() === '--') {
+		return null;
+	}
+
 	// Npm requires script arguments with dashes, like so: `npm run test -- --watch`
 	// Yarn results in a deprecation warning in this case, so dashes need to be skipped
-	const shouldPrefixArgs = rawArgs.length > 0 && !isYarn(npmExec);
-	return shouldPrefixArgs ? ['--', ...rawArgs] : rawArgs;
-}
-
-function run(args, isCi, spawn, npmExec = 'npm') {
-	const script = isCi ? args[0] : args[1];
-	const scriptArgs = getScriptArgs(args, npmExec);
-
-	if (script && script.trim() !== '--') {
-		return spawn(npmExec, ['run', script, ...scriptArgs], {
-			stdio: 'inherit',
-		});
+	const shouldPrefixArgs = additionalArgs.length > 0 && !isYarn(npmExec);
+	if (shouldPrefixArgs) {
+		additionalArgs.unshift('--');
 	}
+
+	return ['run', script, ...additionalArgs];
 }
 
-module.exports = run;
+module.exports = {
+	// Just for testing :)
+	_getSpawnArgs: getSpawnArgs,
+};
 
 if (require.main === module) {
-	const child = run(
-		process.argv.slice(2),
-		isCi,
-		crossSpawn,
-		process.env.npm_execpath
-	);
-	if (child) {
+	const npmExec = process.env.npm_execpath || 'npm';
+	const spawnArgs = getSpawnArgs(process.argv.slice(2), isCi, npmExec);
+	if (spawnArgs) {
+		const child = spawn(npmExec, spawnArgs, {
+			stdio: 'inherit',
+		});
+
 		child.on('exit', process.exit);
 	}
 }
--- test.js
+++ test.js
@@ -1,14 +1,9 @@
 const test = require('ava');
-const sinon = require('sinon');
-const run = require('./cli');
-
-const mockSpawn = () => sinon.fake();
+const getSpawnArgs = require('./cli')._getSpawnArgs;
 
 const macro = (isCi) => (t, input, expected, npmExec = 'npm') => {
-	const spawn = mockSpawn();
-	run(input.split(/\s+/), isCi, spawn, npmExec);
-	const actual = spawn.lastCall.args.slice(0, 2);
-	t.deepEqual(actual, [npmExec, ['run', ...expected.split(/\s+/)]]);
+	const actual = getSpawnArgs(input.split(/\s+/), isCi, npmExec);
+	t.deepEqual(actual, ['run', ...expected.split(/\s+/)]);
 };
 
 const ci = macro(true);
@@ -51,13 +46,9 @@ test(
 );
 
 test('no ci: runs nothing if no second script defined', (t) => {
-	const spawn = mockSpawn();
-	run(['first'], false, spawn);
-	t.false(spawn.called);
+	t.is(getSpawnArgs(['first'], false), null);
 });
 
 test('no ci: runs nothing if no second script defined but has arguments', (t) => {
-	const spawn = mockSpawn();
-	run(['first', '--', '--test', 'value'], false, spawn);
-	t.false(spawn.called);
+	t.is(getSpawnArgs(['first'], false), null);
 });
